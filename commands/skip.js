16964fedb5bb9292fd46d4eab5d46b1eb5444347
Raúl <raul.parada040103@gmail.com>
2024-10-28 15:14:13 +0100

    /stop y /skip funcionando con discord-player

--- commands/skip.js
+++ commands/skip.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { useMainPlayer } = require('discord-player');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,8 +7,8 @@ module.exports = {
         .setDescription("Salta la canción actual"),
 
     run: async ({ interaction }) => {
-        const { member, client } = interaction;
-
+        const { member } = interaction;
+        const player = useMainPlayer();
         const voiceChannel = member.voice.channel;
         const embed = new EmbedBuilder();
 
@@ -15,20 +16,21 @@ module.exports = {
             embed.setColor("Red").setDescription("¡Debes estar en el canal de voz para usar este comando!");
             return interaction.followUp({ embeds: [embed] });
         } else {
-            const queue = client.distube.getQueue(voiceChannel);
-            if (!queue || !queue.songs.length) {
+            const queue = player.nodes.get(interaction.guild.id)
+            if (!queue) {
                 embed.setColor("Red").setDescription("No hay ninguna canción reproduciéndose en este momento");
                 return await interaction.followUp({ embeds: [embed] });
-            } else if (queue.songs.length == 1) {
-                await queue.stop(voiceChannel);
-                embed.setColor("Red").setDescription("Sea saltado la unica canción puesta");
-                return await interaction.followUp({ embeds: [embed] });
+                /*} else if (queue.songs.length == 1) {
+                    await queue.stop(voiceChannel);
+                    embed.setColor("Red").setDescription("Sea saltado la unica canción puesta");
+                    return await interaction.followUp({ embeds: [embed] });*/
             } else {
                 try {
-                    await queue.skip(voiceChannel);
+                    queue.node.skip();
                 } catch (error) {
+                    console.log(error);
                     embed.setColor("Red").setDescription("Hubo un error al intentar skipear la canción");
-                    await interaction.followUp({ embeds: [embed] });
+                    return await interaction.followUp({ embeds: [embed] });
                 }
 
                 embed.setColor("Green").setDescription("✅ Canción skipeada con éxito");
--- commands/stop.js
+++ commands/stop.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { useMainPlayer } = require('discord-player');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,25 +7,29 @@ module.exports = {
         .setDescription("Para la canción actual"),
 
     run: async ({ interaction }) => {
-        const { member, client } = interaction;
-
+        const player = useMainPlayer();
+        const { member } = interaction;
         const voiceChannel = member.voice.channel;
         const embed = new EmbedBuilder();
+
+        console.log(useMainPlayer());
 
         if (!voiceChannel) {
             embed.setColor("Red").setDescription("¡Debes estar en el canal de voz para usar este comando!");
             return interaction.followUp({ embeds: [embed] });
         } else {
-            const queue = client.distube.getQueue(voiceChannel);
-            if (!queue || !queue.songs.length) {
+            const queue = player.nodes.get(interaction.guild.id)
+
+            if (!queue) {
                 embed.setColor("Red").setDescription("No hay ninguna canción reproduciéndose en este momento");
                 return await interaction.followUp({ embeds: [embed] });
             } else {
                 try {
-                    await queue.stop(voiceChannel);
+                    queue.node.stop(false);
                 } catch (error) {
+                    console.log(error);
                     embed.setColor("Red").setDescription("Hubo un error al intentar parar la canción");
-                    await interaction.followUp({ embeds: [embed] });
+                    return await interaction.followUp({ embeds: [embed] });
                 }
 
                 embed.setColor("Green").setDescription("✅ Canción parada con éxito");
