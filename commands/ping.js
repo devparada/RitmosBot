aadbbb28d2e290f28f117b6c8be6127a3d283d55
Raúl <raul.parada040103@gmail.com>
2024-11-09 01:20:48 +0100

    Mejorado el /ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,26 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
-        .setDescription("Mide la latencia del bot"),
+        .setDescription("Mide la latencia del bot y de la API de Discord"),
 
     run: async ({ interaction }) => {
         const fecha = Date.now();
-        await interaction.reply("🏓 Pong!");
+        await interaction.reply("🏓 Pong! Calculando latencia...");
 
-        const latencia = Date.now() - fecha;
-        await interaction.editReply(`🏓 Pong! Latencia: ${latencia}ms`);
+        const botLatencia = Date.now() - fecha;
+        const apiLatencia = interaction.client.ws.ping;
+
+        const embed = new EmbedBuilder()
+            .setColor("Random")
+            .setTitle("🏓 Pong!")
+            .setDescription("Tiempos de respuesta:")
+            .addFields(
+                { name: "Latencia del bot", value: `${botLatencia}ms`, inline: true },
+                { name: "Latencia de la API", value: `${apiLatencia}ms`, inline: true }
+            )
+
+        await interaction.editReply({ content: null, embeds: [embed] });
     }
 }
