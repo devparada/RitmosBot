330a4ed916733b90a0d773a464a93cda07049c4a
Raúl <raul.parada040103@gmail.com>
2024-12-01 23:34:04 +0100

    Modificadas las reglas + limpieza dependencia

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,35 +15,14 @@ export default [
       },
     },
     rules: {
-      // Requiere punto y coma al final de cada línea
-      semi: ["warn", "always"],
-
-      // Usa comillas dobles en lugar de simples para cadenas de texto
-      "quotes": ["warn", "double"],
-
-      // Asegura que haya una línea en blanco al final de cada archivo
-      "eol-last": ["error", "always"],
-
-      // Exige coma final en objetos y arrays multilínea para facilitar ediciones futuras
-      "comma-dangle": ["warn", "always-multiline"],
-
-      // Obliga el uso de === y !== para evitar conversiones implícitas de tipo
-      "eqeqeq": ["error", "always"],
-
-      // Advierte sobre variables definidas pero no utilizadas, ignorando argumentos no usados
-      "no-unused-vars": ["warn", { "args": "none", "ignoreRestSiblings": true }],
-
-      // Prohíbe el uso de variables no definidas para prevenir errores
-      "no-undef": "error",
-
-      // Prohíbe espacios en blanco innecesarios al final de las líneas
-      "no-trailing-spaces": "error",
-
-      // Exige espacios consistentes antes y después de las flechas en funciones flecha
-      "arrow-spacing": ["error", { "before": true, "after": true }],
-
-      // Asegura un espaciado consistente alrededor de los dos puntos en los objetos
-      "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
+      semi: ["warn", "always"], // Requiere punto y coma al final
+      "quotes": ["warn", "double"], // Usa comillas dobles en lugar de simples para cadenas de texto
+      "eol-last": ["warn", "always"], // Asegura que haya una línea en blanco al final de cada archivo
+      "comma-dangle": ["warn", "always-multiline"], // Exige coma final en objetos y arrays multilínea para facilitar ediciones futuras
+      "eqeqeq": ["error", "always"], // Obliga el uso de === y !== para evitar conversiones implícitas de tipo
+      "no-unused-vars": ["warn", { "args": "none", "ignoreRestSiblings": true }], // Advierte sobre variables definidas pero no utilizadas
+      "no-undef": "error", // Prohíbe el uso de variables no definidas para prevenir errores
+      "no-trailing-spaces": "error", // Prohíbe espacios en blanco innecesarios al final de las líneas
     },
   },
 ];
