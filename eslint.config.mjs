84e777f8243379342d53ac30a6dc996d512f28c3
Raúl <raul.parada040103@gmail.com>
2025-05-24 15:24:14 +0200

    Añadido y configurado Prettier + añadido script format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier/flat";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
+
   {
     files: ["**/*.js"],
     languageOptions: {
@@ -14,15 +16,21 @@ export default [
         ...globals.jest, // Variables globales de Jest
       },
     },
+    plugins: {
+      prettier: pluginPrettier,
+    },
     rules: {
       semi: ["warn", "always"], // Requiere punto y coma al final
-      "quotes": ["warn", "double"], // Usa comillas dobles en lugar de simples para cadenas de texto
+      quotes: ["warn", "double"], // Usa comillas dobles en lugar de simples para cadenas de texto
       "eol-last": ["warn", "always"], // Asegura que haya una línea en blanco al final de cada archivo
       "comma-dangle": ["warn", "always-multiline"], // Exige coma final en objetos y arrays multilínea para facilitar ediciones futuras
-      "eqeqeq": ["error", "always"], // Obliga el uso de === y !== para evitar conversiones implícitas de tipo
-      "no-unused-vars": ["warn", { "args": "none", "ignoreRestSiblings": true }], // Advierte sobre variables definidas pero no utilizadas
+      eqeqeq: ["error", "always"], // Obliga el uso de === y !== para evitar conversiones implícitas de tipo
+      "no-unused-vars": ["warn", { args: "none", ignoreRestSiblings: true }], // Advierte sobre variables definidas pero no utilizadas
       "no-undef": "error", // Prohíbe el uso de variables no definidas para prevenir errores
       "no-trailing-spaces": "warn", // Asegura que no haya espacios en blanco innecesarios al final de las líneas
     },
   },
+
+  // Desactivamos cualquier regla de ESLint que choque con Prettier
+  configPrettier,
 ];
