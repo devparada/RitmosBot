966b3d50e06410be0543bbf4a28e1d7956cfa6d6
Raúl <raul.parada040103@gmail.com>
2025-06-22 15:11:40 +0200

    Solucionado warning de ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,12 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
-    globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.test.json"
-    }
-  },
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                tsconfig: "tsconfig.test.json",
+            },
+        ],
+    },
 };
