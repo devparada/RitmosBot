9e9570c21f9bebfffe56a6b8c72f0c4a4a324216
Raúl <raul.parada040103@gmail.com>
2025-06-22 12:43:27 +0200

    Añadidas las dependencias ts-jest y config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+    globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json"
+    }
+  },
+};
