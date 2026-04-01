b9ece8a78ca035028d1b423bc9ace7233d12a594
Raúl <raul.parada040103@gmail.com>
2026-04-01 23:04:52 +0200

    chore: Uso de Lavalink en leave, queue y skip + correcciones en ping

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -1,29 +1,30 @@
 import { type ChatInputCommandInteraction, Colors, EmbedBuilder, SlashCommandBuilder } from "discord.js";
-import { useMainPlayer } from "discord-player";
+import type { ExtendedClient } from "@/types/discord";
 import { usuarioEnVoiceChannel } from "@/utils/voiceUtils";
 
 module.exports = {
     data: new SlashCommandBuilder().setName("leave").setDescription("Desconecta el bot del chat de voz"),
 
-    run: async ({ interaction }: { interaction: ChatInputCommandInteraction }) => {
+    run: async ({ client, interaction }: { client: ExtendedClient; interaction: ChatInputCommandInteraction }) => {
         const embed = new EmbedBuilder();
 
         // Verifica si el usuario está en un canal de voz
         if (!usuarioEnVoiceChannel) {
             return false;
-        } else {
-            const player = useMainPlayer();
-            if (interaction.guild != null) {
-                const queue = player.nodes.get(interaction.guild.id);
+        }
 
-                if (!queue?.connection) {
-                    embed.setColor(Colors.Red).setDescription("❌ No estoy conectado a ningún canal de voz");
-                } else {
-                    queue.delete();
-                    embed.setColor(Colors.Green).setDescription("✅ Me he desconectado del canal de voz");
-                }
+        if (interaction.guildId) {
+            const player = client.lavalink.getPlayer(interaction.guildId);
+
+            if (!player?.connected) {
+                embed.setColor(Colors.Red).setDescription("❌ No estoy conectado a ningún canal de voz.");
+            } else {
+                await player.destroy();
+
+                embed.setColor(Colors.Green).setDescription("✅ Me he desconectado del canal de voz.");
             }
         }
-        return interaction.reply({ embeds: [embed] });
+
+        return interaction.editReply({ embeds: [embed] });
     },
 };
